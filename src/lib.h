c267f7f43c483d1305cad35bf5d1f527a000604a
Fedor P. Goncharov <gfederix@gmail.com>
2020-07-14 02:29:33 +0700

    move function return to double

--- src/lib.h
+++ src/lib.h
@@ -5,11 +5,17 @@ int return_zero(){
 }
 
 template <typename T>
-T mean(std::vector<T> const & v){
-  return std::accumulate(v.begin(), v.end(), 0) / v.size();
+double mean(std::vector<T> const & v){
+  return std::accumulate(v.begin(), v.end(), 0.) / static_cast<double> (v.size());
 }
 
 template <typename T>
-int median (std::initializer_list<T> v) {
-  return 0;
+double median (std::vector<T> v) {
+  std::sort(v.begin(), v.end());
+  auto s = v.size();
+  if (s % 2){
+    return v[s / 2];
+  }
+  int second_pos = s / 2;
+  return (v[second_pos - 1] + v[second_pos]) / 2.;
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,18 +1,24 @@
 #include "gtest/gtest.h"
 #include "lib.h"
 
-namespace {
+namespace testing{
 
   TEST(ZeroTest, Zero) {
     EXPECT_EQ(0, return_zero());
   }
-  TEST(MeanTest, Zero) {
-    EXPECT_EQ(0, mean(std::vector {0}));
-    EXPECT_EQ(1, mean(std::vector {1, 1, 1, 1}));
-    EXPECT_EQ(2, mean(std::vector {1, 4}));
-    EXPECT_EQ(2., mean(std::vector {1., 4.}));
+  TEST(MeanTest, Int) {
+    EXPECT_EQ(0., mean(std::vector {0}));
+    EXPECT_EQ(1., mean(std::vector {1, 1, 1, 1}));
+    EXPECT_EQ(2.5, mean(std::vector {1, 4}));
+    EXPECT_EQ(2.5, mean(std::vector {1., 4.}));
   }
-  TEST(MeanFloatTest, Zero) {
-    EXPECT_EQ(2., mean(std::vector {1., 4.}));
+  TEST(MeanTest, Float) {
+    EXPECT_EQ(2.5, mean(std::vector {1., 4.}));
+  }
+  TEST(MedianTest, Int) {
+    EXPECT_EQ(0., median(std::vector {0}));
+    EXPECT_EQ(1., median(std::vector {1}));
+    EXPECT_EQ(2., median(std::vector {1, 2, 3}));
+    EXPECT_EQ(2.5, median(std::vector {1, 2, 3, 4}));
   }
 }
